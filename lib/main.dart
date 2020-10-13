b77ab8f2b533b8c24476fa0ddd7f6d6894348d9b
Lenz Paul <lenz@pop-os.localdomain>
2020-10-13 07:58:56 -0700

    bottom navbar done

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,51 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return (Scaffold(
+      bottomNavigationBar: buildBottomNavigationBar(),
       body: Text('gg'),
     ));
+  }
+
+  BottomNavigationBar buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      selectedItemColor: Colors.red,
+      // backgroundColor: bottomBackgroundColor,
+      unselectedItemColor: Colors.grey[400],
+      elevation: 4,
+      type: BottomNavigationBarType.fixed,
+      iconSize: 35,
+      items: [
+        BottomNavigationBarItem(
+          title: Text(''),
+          icon: Icon(
+            Icons.home,
+          ),
+        ),
+        BottomNavigationBarItem(
+          title: Text(''),
+          icon: Icon(
+            Icons.favorite_border,
+          ),
+        ),
+        BottomNavigationBarItem(
+          title: Text(''),
+          icon: Icon(
+            Icons.location_city,
+          ),
+        ),
+        BottomNavigationBarItem(
+          title: Text(''),
+          icon: Icon(
+            Icons.shopping_cart,
+          ),
+        ),
+        BottomNavigationBarItem(
+          title: Text(''),
+          icon: Icon(
+            Icons.person_outline,
+          ),
+        )
+      ],
+    );
   }
 }
