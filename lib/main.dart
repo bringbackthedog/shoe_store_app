4f9b3df6f6ff5c3022f8bb1b4b44297fecaf12da
Lenz Paul <lenz@pop-os.localdomain>
2020-10-13 09:21:30 -0700

    broke some...

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const brands = [
+  'Nike',
+  'Adidas',
+  'Jordan',
+  'Puma',
+  'Reebok',
+  'Timberland',
+  'Vans',
+  'Converse'
+];
 
 void main() {
-  runApp(MaterialApp(
-    home: MyApp(),
-  ));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return (Scaffold(
-      bottomNavigationBar: buildBottomNavigationBar(),
-      body: Text('gg'),
-    ));
+    return MaterialApp(
+      // final size = MediaQuery.of(context).size;
+      home: Scaffold(
+        appBar: buildAppBar(context),
+        bottomNavigationBar: buildBottomNavigationBar(),
+        body: SizedBox.expand(
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 15.0,
+                  vertical: 0.0,
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      // width: constraints.maxWidth,
+                      height: constraints.maxHeight,
+                      // height: size.height * 0.2,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 5,
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.only(right: 20.0),
+                              child: Card(
+                                child: Text(brands[index]),
+                              ),
+                            );
+                          }),
+                    )
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      elevation: 0.0,
+      backgroundColor: Colors.transparent,
+      textTheme: Theme.of(context).textTheme.copyWith(),
+      title: Text(
+        'Discover',
+        style: GoogleFonts.roboto(fontSize: 30, fontWeight: FontWeight.bold),
+      ),
+      actions: [
+        CircleAvatar(child: Icon(Icons.search)),
+        SizedBox(
+          width: 18.0,
+        ),
+        CircleAvatar(child: Icon(Icons.notifications_none)),
+      ],
+    );
   }
 
   BottomNavigationBar buildBottomNavigationBar() {
